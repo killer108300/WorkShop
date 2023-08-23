dae1d891beb98a6fa02fa005709626c79f5cdf18
Zidarc <121773452+Zidarc@users.noreply.github.com>
2023-08-24 04:22:22 +0500

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,19 @@
+s = "{[()]}"
+stack_openpar = []
+def pop(input_stack):
+    input_stack = input_stack.remove()
+def push(input_stack, open_index):
+    input_stack.append(open)
+dicts = {
+    "(" : ")",
+    "[" : "]",
+    "{" : "}"
+}
+for x in range(len(s)):
+    if dicts.get(s[x]) is not None:
+        #open
+        push(stack_openpar,s[x])
+    elif dicts.get(s[x]) is None:
+        #close
+        if Exception:
+            pass
